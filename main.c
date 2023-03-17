5cd4fe2ff7f1683ff2734ce3be6a995ec3a18e0d
Pacruz20 <101412730+Pacruz20@users.noreply.github.com>
2023-03-17 12:20:35 -0300

    ahora hay texto + mas ordenado

--- main.c
+++ main.c
@@ -2,13 +2,17 @@
 // Created by pabli on 17/3/2023.
 //
 
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 int main(){
     int a;
-printf("\nHola mundo");
-printf("\n Ingrese un numero:");
-scanf("%d",&a);
-printf("el numero ingresado es: %d",a);
-return 0;
+    char b[100];
+    printf("Hola mundo");
+    printf("\nIngrese un numero:");
+    scanf("%d",&a);
+    printf("\nIngrese un texto:");
+    scanf("%s",&b);
+    printf("\nEl numero ingresado es: %d\n",a);
+    printf("\nEl texto ingresado es: %s\n",b);
+    return 0;
 };
