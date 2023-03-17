271d5548bd494aa9d40441725810a5e6ac3ff94e
Pacruz20 <101412730+Pacruz20@users.noreply.github.com>
2023-03-17 11:52:11 -0300

    commit 1

---
+++ main.c
@@ -0,0 +1,10 @@
+//
+// Created by pabli on 17/3/2023.
+//
+
+#include "main.h"
+#include <stdio.h>
+int main(){
+printf("\nHola mundo");
+return 0;
+};
---
+++ main.h
@@ -0,0 +1,10 @@
+//
+// Created by pabli on 17/3/2023.
+
+
+//
+
+#ifndef HOLA_MUNDO_EDYA_MAIN_H
+#define HOLA_MUNDO_EDYA_MAIN_H
+
+#endif //HOLA_MUNDO_EDYA_MAIN_H
